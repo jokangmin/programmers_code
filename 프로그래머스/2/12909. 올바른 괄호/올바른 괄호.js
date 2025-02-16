c26290606ec80070be2df9b421d2714f099a59b6
jokangmin <145452528+jokangmin@users.noreply.github.com>
2025-02-16 17:27:31 +0900

    [level 2] Title: 올바른 괄호, Time: 3.56 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,12 @@
+function solution(s){
+    var balance = 0;
+    for(let i=0;i<s.length;i++){
+        if(s[i] === "("){
+            balance++;
+        }else{
+            balance--;
+            if (balance < 0) return false;
+        }
+    }
+    return balance === 0;
+}
