92175b9261fef903cc79d602b285ee7c39aa8921
jokangmin <145452528+jokangmin@users.noreply.github.com>
2025-03-22 17:41:07 +0900

    [level 3] Title: 네트워크, Time: 0.42 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,22 @@
+function solution(n, computers) {
+    var answer = 0;
+    var visited = Array(n).fill(false);
+
+    function dfs(index) {
+        visited[index] = true;
+        for(let i=0;i<n;i++) {
+            if(computers[index][i] === 1 && !visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    for(let i=0;i<n;i++) {
+        if(!visited[i]) {
+            dfs(i);
+            answer++;
+        }
+    }
+
+    return answer;
+}
