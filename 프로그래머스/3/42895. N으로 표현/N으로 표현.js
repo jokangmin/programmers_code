b0ce27d5d75a364efdf2a790d2cac23c1a98f82a
jokangmin <145452528+jokangmin@users.noreply.github.com>
2025-04-17 16:32:57 +0900

    [level 3] Title: N으로 표현, Time: 23.63 ms, Memory: 39.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -0,0 +1,27 @@
+function solution(N, number) {
+    if (N === number) return 1;
+
+    const dp = Array.from({ length: 9 }, () => new Set());
+
+    for (let i = 1; i <= 8; i++) {
+        const repeated = Number(String(N).repeat(i));
+        dp[i].add(repeated);
+
+        for (let j = 1; j < i; j++) {
+            for (let a of dp[j]) {
+                for (let b of dp[i - j]) {
+                    dp[i].add(a + b);
+                    dp[i].add(a - b);
+                    dp[i].add(b - a);
+                    dp[i].add(a * b);
+                    if (b !== 0) dp[i].add(Math.floor(a / b));
+                    if (a !== 0) dp[i].add(Math.floor(b / a));
+                }
+            }
+        }
+
+        if (dp[i].has(number)) return i;
+    }
+
+    return -1;
+}
