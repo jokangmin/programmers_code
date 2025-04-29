b910dec349b4ce7eced7d2a216258b807fd20d34
jokangmin <145452528+jokangmin@users.noreply.github.com>
2025-04-29 17:24:03 +0900

    [level 3] Title: 순위, Time: 14.22 ms, Memory: 38.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -0,0 +1,34 @@
+function solution(n, results) {
+    var answer = 0;
+    const win = Array.from({length: n + 1}, () => []);
+    const lose = Array.from({length: n + 1}, () => []);
+    const count_arr = Array(n+1).fill(0);
+    
+    for(const [a, b] of results){
+        win[a].push(b); 
+        lose[b].push(a);
+    }
+   
+    function dfs(graph, index, visited){
+        for(const item of graph[index]){
+            if(!visited.has(item)){
+                visited.add(item);
+                dfs(graph, item, visited);
+            }
+        }
+    }
+    
+    for(let i=1;i<=n;i++){
+        const winVisited = new Set();
+        const loseVisited = new Set();
+
+        dfs(win, i, winVisited);
+        dfs(lose, i, loseVisited);
+        
+        if(winVisited.size + loseVisited.size === n - 1){
+            answer++;
+        }
+    }
+    
+    return answer;
+}
