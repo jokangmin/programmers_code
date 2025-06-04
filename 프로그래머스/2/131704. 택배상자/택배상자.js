820966310c2b341afe26b51c51321a3405bed1d2
jokangmin <145452528+jokangmin@users.noreply.github.com>
2025-06-04 18:51:21 +0900

    [level 2] Title: 택배상자, Time: 34.32 ms, Memory: 92.7 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.js
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -1,30 +1,25 @@
 function solution(order) {
-    var answer = 0;
-    var sub_belt = [];
-    var now_index = 0;
-    var now_num = 1;
-    
-    while(now_num <= order.length){
-        if(now_num === order[now_index]){
+    let answer = 0;
+    let subBelt = [];
+    let currentBox = 1;
+    let i = 0;
+
+    while (true) {
+        if (currentBox === order[i]) {
             answer++;
-            now_index++;
-        }else{
-            sub_belt.push(now_num);
+            currentBox++;
+            i++;
+        } else if (subBelt.length > 0 && subBelt[subBelt.length - 1] === order[i]) {
+            subBelt.pop();
+            answer++;
+            i++;
+        } else if (currentBox <= order.length) {
+            subBelt.push(currentBox);
+            currentBox++;
+        } else {
+            break;
         }
-        
-        while(sub_belt.length > 0){
-            if(sub_belt[sub_belt.length-1] === order[now_index]){
-                answer++;
-                now_index++;
-                sub_belt.pop();
-            }else{
-                break;
-            }
-        }
-        
-        now_num++;
     }
-    
-    
+
     return answer;
 }
